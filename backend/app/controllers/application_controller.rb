604a42ce6452e324ecef3d9edea48ad8d1998a17
Chepkoech1 <sandrachepkoech60@gmail.com>
2023-03-03 17:28:56 +0300

    added arrays to store pets

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
+  # array to store pets
+  PETS = [
+    { name: 'Fluffy', species: 'cat', breed: 'Persian', age: 3 },
+    { name: 'Max', species: 'dog', breed: 'Golden Retriever', age: 2 },
+    { name: 'Lola', species: 'dog', breed: 'Chihuahua', age: 5 },
+    { name: 'Simba', species: 'lion', breed: 'African Lion', age: 6 },
+    { name: 'Nemo', species: 'fish', breed: 'Clown Fish', age: 1 },
+    { name: 'Coco', species: 'bird', breed: 'Parrot', age: 4 }
+
+  ]
   
   # Add your routes here
   get "/" do
