e94aaa4133516cbb296064dc06ea76e449cd7523
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-03-04 22:21:06 +0300

    removed frontend folder

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,17 +1,13 @@
-
-import './App.css';
 import { Route, Routes} from 'react-router-dom';
 import LoginForm from './components/LoginForm'
 
 function App() {
   return (
-    
     <main>
      <Routes>
       <Route path='/' element={LoginForm}/>
      </Routes>
     </main>
-    
   );
 }
 
