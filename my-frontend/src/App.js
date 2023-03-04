18ef7cfa5f1197bc769ddd494f9040939a1efbab
Kiprotich Shadrack <shadrackr057@gmail.com>
2023-03-04 23:50:44 +0300

    styled log in form

--- my-frontend/src/App.js
+++ my-frontend/src/App.js
@@ -1,18 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import LogIn from './components/LogIn';
-import SignUp from './components/SignUp';
-import Dashboard from './components/Dashboard';
+import LogIn from './pages/LogIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-   <main>
-      <Routes>
-        <Route path='/' element={<LogIn />}/>
-        <Route path='/signUp' element={<SignUp />}/>
-        <Route path='/dashboard' element={<Dashboard />}/>
-      </Routes>
-   </main>
+    <Routes>
+      <Route path='/' element={<LogIn />}/>
+      <Route path='/signUp' element={<SignUp />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+    </Routes>
   );
 }
 
